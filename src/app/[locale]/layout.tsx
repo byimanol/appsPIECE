8843dc0ded8dc410d8bc17aa233924fc623c2db2
unknown <imanol5522@gmail.com>
2024-05-17 19:13:23 -0400

    favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -41,8 +41,15 @@ export default function RootLayout({
       <body className="bg-gray-800 overflow-x-hidden" >
         <header>
 
-
-
+          <link rel="apple-touch-icon" sizes="180x180" href="public/favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="public/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="public/favicon/favicon-16x16.png"/>
+          <link rel="manifest" href="public/favicon/site.webmanifest"/>
+          <link rel="mask-icon" href="public/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+          <link rel="shortcut icon" href="public/favicon/favicon.ico"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
+          <meta name="msapplication-config" content="public/favicon/browserconfig.xml"/>
+          <meta name="theme-color" content="#ffffff"/>
 
           <script async={true} src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2588507502767667"
         crossOrigin="anonymous"></script>
