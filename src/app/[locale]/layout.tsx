7c55b25e28bfdbaffa948eecde062f06f920618f
unknown <imanol5522@gmail.com>
2024-05-17 19:34:30 -0400

    favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -41,14 +41,14 @@ export default function RootLayout({
       <body className="bg-gray-800 overflow-x-hidden" >
         <header>
 
-          <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"/>
-          <link rel="manifest" href="favicon/site.webmanifest"/>
-          <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5"/>
-          <link rel="shortcut icon" href="favicon/favicon.ico"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png"/>
+          <link rel="manifest" href="icons/site.webmanifest"/>
+          <link rel="mask-icon" href="icons/safari-pinned-tab.svg" color="#5bbad5"/>
+          <link rel="shortcut icon" href="icons/favicon.ico"/>
           <meta name="msapplication-TileColor" content="#da532c"/>
-          <meta name="msapplication-config" content="favicon/browserconfig.xml"/>
+          <meta name="msapplication-config" content="icons/browserconfig.xml"/>
           <meta name="theme-color" content="#ffffff"/>
 
           <script async={true} src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2588507502767667"
